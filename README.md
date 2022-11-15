ef6f2c10d7bc6fd6440665a308a6e1057d9baad2
franzannakarolina <franz.karols@gmail.com>
2022-11-14 23:11:09 -0300

    Aprimorando conhecimento em components React utilizando Radix

---
+++ README.md
@@ -0,0 +1,3 @@
+Com Radix os componentes poliformismo podem ser vários elementos, usando slot do Radix, segue docs:
+
+https://www.radix-ui.com/
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/Button";
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <div className="w-screen h-screen bg-zinc-900 flex items-center justify-center gap-4">
+      <Button>Create Account</Button>
+      <Button asChild>
+        <a href="#">Link to website</a>
+      </Button>
+    </div>
+  );
+}
